81d251114a543a426167e009e179c3d80bcaca02
Valter Sjöstrand <Valter.sjo@gmail.com>
2020-04-15 13:00:35 +0200

    Fixed first scene

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,52 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.awt.event.ActionEvent;
+
 public class Controller {
+
+
+
+    @FXML
+    private void loginButtonOnAction(javafx.event.ActionEvent event) {
+        try {
+            Node node = (Node) event.getSource();
+            Stage stage = (Stage) node.getScene().getWindow();
+
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("sampleLogin.fxml"));
+            Parent root = loader.load();
+
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    @FXML
+    private void registerButtonOnAction(javafx.event.ActionEvent event) {
+        try {
+            Node node =(Node) event.getSource();
+            Stage stage = (Stage)node.getScene().getWindow();
+
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("Register.fxml"));
+            Parent root = loader.load();
+
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+
 }
+
