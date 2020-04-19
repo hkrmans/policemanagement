88acdac1d93827542d3a88b108252c44404ce56c
Mats Lundager <51341536+B0gg3@users.noreply.github.com>
2020-04-19 13:43:48 +0200

    Add files via upload

---
+++ src/sample/Sec.java
@@ -0,0 +1,40 @@
+import java.nio.charset.StandardCharsets;
+import java.security.*;
+
+public class Sec{
+    private static final SecureRandom rand = new SecureRandom();
+
+    public void checkString(String input){
+        try{
+            if(input.length() < 12 || input.length() > 40){
+                throw new IllegalArgumentException("Error");
+            }
+            if(input.chars().filter(c-> c>='A' && c<='Z').count() == 0 ||input.chars().filter(c-> c>='0' && c<='9').count() == 0){
+                throw new IllegalArgumentException("Error does not fufill requirements!");
+            }
+        }catch(IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void checkMysql(String input){
+        try{
+            if(input.chars().filter(c-> c>='A' && c<='Z').count() + input.chars().filter(c-> c>='a' && c<='z').count() != input.length()){
+                throw new IllegalArgumentException("Only letters are allowed!");
+            }
+        }catch(IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public StringBuffer hashPassword(String input) throws NoSuchAlgorithmException {
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        byte[] encodedhash = digest.digest(input.getBytes(StandardCharsets.UTF_8));
+        StringBuffer hexString = new StringBuffer();
+        for(Byte e: encodedhash){
+            String hex = Integer.toHexString(0xff& e);
+            hexString.append(hex);
+        }
+        return hexString;
+    }
+}
